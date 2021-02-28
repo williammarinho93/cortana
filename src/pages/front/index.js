48d584dba03fd03bb8fd56eb869d0cd25795d2f6
williammarinho93 <56370582+williammarinho93@users.noreply.github.com>
2021-02-28 18:39:43 -0300

    first commit

---
+++ src/pages/front/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Image, Text} from 'react-native';
+
+
+import styles from './styles';
+
+export default function App() {
+    return (
+      <View style={styles.container}>
+        <Text>Cortana Como posso lhe ajudar?</Text>
+      </View>
+    );
+  }
---
+++ src/pages/front/styles.js
@@ -0,0 +1,19 @@
+import {StyleSheet} from 'react-native';
+
+
+
+
+
+
+export default StyleSheet.create({
+    container: {
+     
+      flex: 1,
+      backgroundColor: '#171717',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+  })
+
+
+
